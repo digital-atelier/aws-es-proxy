75159e41a5dacf0a728d48a6cab6857917cb5a88
Dhruv <dhruva@servana.net>
2019-04-26 10:21:11 +0530

    Add refresh credentails functionality to the code

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const AWS = require('aws-sdk');
 const co = require('co');
 const url = require('url');
 const http = require('http');
-const options = require('optimist').argv;
+const options = require('optimist')
+  .argv;
 
 const context = {};
 process.env.AWS_PROFILE = process.env.AWS_PROFILE || options.profile || 'default';
-var creds = {};
+
 
 var updateCredentials = function () {
   return new Promise(function (resolve, reject) {
@@ -21,60 +22,67 @@ var updateCredentials = function () {
   });
 }
 
-var execute = function (endpoint, region, path, method, body) {
+
+var execute = function(endpoint, region, path, headers, method, body) {
   return new Promise((resolve, reject) => {
-    co(function* () {
+  co(function* () {
+    var req = new AWS.HttpRequest(endpoint);
 
-        var req = new AWS.HttpRequest(endpoint);
-        console.log('AWS HTTP Request:', method, path);
+    if(options.quiet !== true) {
+      console.log('>>>', method, path);
+    }
 
-        req.method = method || 'GET';
-        req.path = path;
-        req.region = region;
+    req.method = method || 'GET';
+    req.path = path;
+    req.region = region;
+    req.body = body;
 
-        if(body) {
-          if(typeof body === "object") {
-            req.body = JSON.stringify(body);
-          } else {
-            req.body = body;
-          }
-        }
+    req.headers['presigned-expires'] = false;
+    req.headers.Host = endpoint.host;
 
-        // Sense likes send GET request with body, which aws-sdk doesn't really allow. Translate to POST instead.
-        if(req.body && req.method == 'GET') {
-          req.method = 'POST';
-        }
+    // Some credentials may require refresh, updateCredentials handles this
+    yield updateCredentials()
 
-        req.headers['presigned-expires'] = false;
-        req.headers.Host = endpoint.host;
+    var signer = new AWS.Signers.V4(req, 'es');
+    signer.addAuthorization(AWS.config.credentials, new Date());
 
-        // Some credentials may require refresh, updateCredentials handles this
-        yield updateCredentials()
-        var signer = new AWS.Signers.V4(req, 'es');
-        signer.addAuthorization(AWS.config.credentials, new Date());
+    // Now we have signed the "headers", we add extra headers passing
+    // from the browser.  We must strip any connection control, transport encoding
+    // incorrect Origin headers, and make sure we don't change the Host header from
+    // the one used for signing
+    var keys = Object.keys(headers)
+    for (var i = 0, len = keys.length; i < len; i++) {
+      if (keys[i] != "host" && 
+        keys[i] != "accept-encoding" &&
+        keys[i] != "connection" &&
+        keys[i] != "origin") {
+        req.headers[keys[i]] = headers[keys[i]];
+      }
+    }
 
-        var send = new AWS.NodeHttpClient();
-        send.handleRequest(req, null, (httpResp) => {
-          var body = '';
-          httpResp.on('data', (chunk) => {
-            body += chunk;
-          });
-          httpResp.on('end', (chunk) => {
-            resolve({
-              statusCode: httpResp.statusCode,
-              body: body
-            });
-          });
-        }, (err) => {
-          console.log('Error: ' + err);
-          reject(err);
+    var client = new AWS.NodeHttpClient();
+    client.handleRequest(req, null, (httpResp) => {
+      var body = '';
+      httpResp.on('data', (chunk) => {
+        body += chunk;
+      });
+      httpResp.on('end', (chunk) => {
+        resolve({
+          statusCode: httpResp.statusCode,
+          headers: httpResp.headers,
+          body: body
         });
-      })
-      .catch(err => reject(err))
+      });
+    }, (err) => {
+      console.log('Error: ' + err);
+      reject(err);
+    });
+  })
+  .catch(err => reject(err))
   });
 };
 
-var readBody = function (request) {
+var readBody = function(request) {
   return new Promise(resolve => {
     var body = [];
 
@@ -83,13 +91,12 @@ var readBody = function (request) {
     });
 
     request.on('end', _ => {
-      console.log('end:', body.length);
       resolve(Buffer.concat(body).toString());
     });
   });
 };
 
-var requestHandler = function (request, response) {
+var requestHandler = function(request, response) {
   var body = [];
 
   request.on('data', chunk => {
@@ -98,7 +105,6 @@ var requestHandler = function (request, response) {
 
   request.on('end', _ => {
     var buf = Buffer.concat(body).toString();
-    console.log('Body:', buf);
 
     co(function*(){
         return yield execute(context.endpoint, context.region, request.url, request.headers, request.method, buf);
@@ -131,17 +137,18 @@ var requestHandler = function (request, response) {
 
 var server = http.createServer(requestHandler);
 
-var startServer = function () {
+var startServer = function() {
   return new Promise((resolve) => {
-    server.listen(context.port, function () {
+    server.listen(context.port, function(){
       console.log('Listening on', context.port);
       resolve();
     });
   });
 };
 
-var main = function () {
-  co(function* () {
+
+var main = function() {
+  co(function*(){
       var maybeUrl = options._[0];
       context.region = options.region || 'eu-west-1';
       context.port = options.port || 9200;
@@ -152,7 +159,8 @@ var main = function () {
         console.log('Options:');
         console.log("\t--profile \tAWS profile \t(Default: default)");
         console.log("\t--region \tAWS region \t(Default: eu-west-1)");
-        console.log("\t--port \tLocal port \t(Default: 9200)");
+        console.log("\t--port  \tLocal port \t(Default: 9200)");
+        console.log("\t--quiet  \tLog less");
         process.exit(1);
       }
 
